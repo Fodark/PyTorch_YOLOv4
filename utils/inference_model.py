31a57c983413eaa4e00d1a7b8817e5a609c833a6
Nicola Dall'Asen <fodark@protonmail.com>
2021-03-22 13:50:44 +0100

    create class for inference yolo

---
+++ utils/inference_model.py
@@ -0,0 +1,36 @@
+import torch
+from models.yolo import Model
+from utils.utils import (
+    coco80_to_coco91_class, check_file, check_img_size, compute_loss, non_max_suppression,
+    scale_coords, xyxy2xywh, clip_coords, plot_images, xywh2xyxy, box_iou, output_to_target, ap_per_class)
+
+
+class YoloMish:
+    def __init__(self, cfg="models/yolov4l-mish.yaml", weights="weights/yolov4l-mish.pt", conf_thresh=.3, iou_thresh=.65):
+        self.conf_thresh = conf_thresh
+        self.iou_thresh = iou_thresh
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = Model(cfg).to(self.device)
+        ckpt = torch.load(weights, map_location=self.device)  # load checkpoint
+        exclude = ['anchor']  # exclude keys
+        ckpt['model'] = {k: v for k, v in ckpt['model'].float().state_dict().items()
+                         if k in self.model.state_dict() and not any(x in k for x in exclude)
+                         and self.model.state_dict()[k].shape == v.shape}
+        self.model.load_state_dict(ckpt['model'], strict=False)
+        print('Transferred %g/%g items from %s' % (len(ckpt['model']), len(self.model.state_dict()), weights))
+
+    def new_predict(self, data):
+        self.model.eval()
+        nb, _, height, width = data.shape  # batch size, channels, height, width
+        inf_out, train_out = self.model(data)
+        output = non_max_suppression(inf_out, conf_thres=self.conf_thresh, iou_thres=self.iou_thresh, merge=False)
+
+        bboxes = []
+        confidences = []
+        class_ids = []
+        for *bbox, conf, class_id in output[0].tolist():
+            bboxes.append([round(b, 3) for b in bbox])
+            confidences.extend([round(conf, 5)])
+            class_ids.extend([int(class_id)])
+
+        return bboxes, confidences, class_ids
