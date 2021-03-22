9c3a4c3bb3430c4a2fb978238c26afc941b4e1b4
Nicola Dall'Asen <fodark@protonmail.com>
2021-03-22 08:13:40 +0100

    format output

--- utils/load_model.py
+++ utils/load_model.py
@@ -18,22 +18,30 @@ def load_model(cfg="models/yolov4l-mish.yaml", weights="weights/yolov4l-mish.pt"
 
     def predict(data, conf_thresh=.3, iou_thresh=.65, merge=False):
         model.eval()
-        jdict = []
+        # jdict = []
         nb, _, height, width = data.shape  # batch size, channels, height, width
         inf_out, train_out = model(data)
         output = non_max_suppression(inf_out, conf_thres=conf_thresh, iou_thres=iou_thresh, merge=merge)
 
-        for si, pred in enumerate(output):
-            clip_coords(pred, (height, width))
-            box = pred[:, :4].clone()  # xyxy
-            #scale_coords([3, height, width], box, height, width)  # to original shape
-            box = xyxy2xywh(box)  # xywh
-            box[:, :2] -= box[:, 2:] / 2  # xy center to top-left corner
-            for p, b in zip(pred.tolist(), box.tolist()):
-                jdict.append({'class_id': int(p[5]),
-                              'bbox': [round(x, 3) for x in b],
-                              'score': round(p[4], 5)})
+        bboxes = []
+        confidences = []
+        class_ids = []
+        for *bbox, conf, class_id in output[0].tolist():
+            bboxes.append(bbox)
+            confidences.extend([round(conf, 5)])
+            class_ids.extend([int(class_id)])
 
-        return jdict
+        # for si, pred in enumerate(output):
+        #    clip_coords(pred, (height, width))
+        #    box = pred[:, :4].clone()  # xyxy
+        #    # scale_coords([3, height, width], box, height, width)  # to original shape
+        #    box = xyxy2xywh(box)  # xywh
+        #    box[:, :2] -= box[:, 2:] / 2  # xy center to top-left corner
+        #    for p, b in zip(pred.tolist(), box.tolist()):
+        #        jdict.append({'class_id': int(p[5]),
+        #                      'bbox': [round(x, 3) for x in b],
+        #                      'score': round(p[4], 5)})
+
+        return bboxes, confidences, class_ids
 
     return predict
